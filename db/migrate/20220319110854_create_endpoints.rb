579643c642594dd33b64a1ba35d853cd9422506c
zaptot <zaptot@gmail.com>
2022-03-19 17:06:44 +0300

    move to sqlite

--- db/migrate/20220319110854_create_endpoints.rb
+++ db/migrate/20220319110854_create_endpoints.rb
@@ -1,21 +1,12 @@
 class CreateEndpoints < ActiveRecord::Migration[6.1]
-  def up
-    execute <<-SQL
-      CREATE TYPE request_method AS ENUM ('post', 'get', 'put', 'delete', 'patch', 'options');
-    SQL
-
-    create_table :endpoints, id: :bigserial do |t|
+  def change
+    create_table :endpoints do |t|
       t.string :path, null: false
-      t.jsonb :response_data, null: false, default: {}
+      t.json :response_data, null: false, default: {}
       t.integer :response_code, null: false, default: 200
-      t.column :http_method, :request_method, null: false
+      t.string :http_method, null: false
 
       t.index %i[path http_method], unique: true
     end
-  end
-
-  def down
-    drop_table :endpoints
-    execute 'DROP TYPE request_method;'
   end
 end
--- db/migrate/20220319110906_create_requests.rb
+++ db/migrate/20220319110906_create_requests.rb
@@ -1,8 +1,8 @@
 class CreateRequests < ActiveRecord::Migration[6.1]
   def change
     create_table :requests do |t|
-      t.jsonb :params, null: false, default: {}
-      t.jsonb :headers
+      t.json :params, null: false, default: {}
+      t.json :headers
       t.string :http_method
       t.references :endpoint
       t.datetime :created_at, null: false
